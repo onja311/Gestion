c6ea9e9d6c6cf884db2320a084dc5b5a0e85b999
Onja <harisoaonja06@gmail.com>
2025-05-06 18:16:37 +0300

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("revenue").textContent = "500 000 MGA";
+    document.getElementById("expenses").textContent = "120 000 MGA";
+    document.getElementById("stock").textContent = "80 unités";
+});
